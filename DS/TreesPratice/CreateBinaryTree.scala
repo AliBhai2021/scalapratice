41f3358ebb32db350dc32c42b4262b83cedac7ab
Lee <90309531+AliBhai2021@users.noreply.github.com>
2023-08-31 11:03:32 +0530

    Add files via upload

--- DS/TreesPratice/CreateBinaryTree.scala
+++ DS/TreesPratice/CreateBinaryTree.scala
@@ -1,82 +1,46 @@
-package TreesPratice
+package DS.BT
 
+import scala.collection.mutable.ListBuffer
 
 trait BinaryTree
-case class BranchNode(lbt:BinaryTree,value:Int,rbt:BinaryTree) extends BinaryTree
-object Leaf extends BinaryTree
+case class Node(lt:BinaryTree, value:Int, rt:BinaryTree) extends BinaryTree
+case object Leaf extends BinaryTree
 
 object CreateBinaryTree extends App{
 
-
-  def createCompleteBinaryTree(value: Int, depth: Int): BinaryTree = {
-    if(depth == 0) Leaf
-    else BranchNode(createCompleteBinaryTree(2 * value,
-      depth - 1),value,  createCompleteBinaryTree(2 * value + 1,
-      depth -1))
-  }
-
-
-  def createAutoBST(arr:Array[Int],indx:Int=0, bt:BinaryTree=Leaf):BinaryTree={
-    println("indx :"+indx+ " element ="+arr(indx)+ "  BT="+bt)
-    indx match {
-      case x if x < arr.length-1 => createAutoBST(arr,indx+1,addNode(arr(indx),bt)) // using addNode fun()
-      case _ => bt
+  var index = -1
+  def createBinaryTreeUsingArrayWithNegitiveValue(array:Array[Int]):BinaryTree={
+    index +=1
+    index match {
+      case idx if idx > array.length => Leaf
+      case idx if array(idx) == -1 => Leaf
+      case idx =>
+         Node(createBinaryTreeUsingArrayWithNegitiveValue(array), array(idx), createBinaryTreeUsingArrayWithNegitiveValue(array))
     }
   }
+  val array = Array(1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1)
+  val bt1 = createBinaryTreeUsingArrayWithNegitiveValue(array)
+  println("createBinaryTreeUsingArrayWithNegitiveValue : "+bt1)
 
-  def createBinaryTreeUsingArrayEments(arr:Array[Int]):BinaryTree={
-    arr match {
-      case elements if elements.isEmpty => Leaf
-      //case element :: elements =>
-      case elements =>
-        val half = elements.tail.length/2
-        BranchNode(
-          createBinaryTreeUsingArrayEments(elements.tail.take(half))
-          ,elements.head
-          ,createBinaryTreeUsingArrayEments(elements.tail.drop(half))
-        )
+  def createBinaryTreeUsingHalfArray(array:Array[Int]):BinaryTree={
+    array match {
+      case  arr if arr.isEmpty => Leaf
+      case arr =>
+        val half = arr.tail.length/2
+        Node(createBinaryTreeUsingHalfArray(arr.tail.take(half)), arr.head, createBinaryTreeUsingHalfArray(arr.tail.drop(half)))
     }
   }
+  val array2 = Array(1,2,3,4,5,6,7)
+  println("createBinaryTreeUsingHalfArray :"+createBinaryTreeUsingHalfArray(array2))
+  println("createBinaryTreeUsingHalfArray :"+createBinaryTreeUsingHalfArray( Array(1,2,3,4,5,6,7,8,9)))
 
-  def addNode(element:Int,bt:BinaryTree):BinaryTree= {
-    bt match {
-      case Leaf => BranchNode(Leaf, element, Leaf)
-      case BranchNode(l, v, r) if element < v => BranchNode(addNode(element, l), v, r)
-      case BranchNode(l, v, r) if element > v => BranchNode(l, v, addNode(element, r))
-      //case BranchNode(l,v,r) if v == element => BranchNode(l,v,l)
-      //case _ => Leaf
+  def createCompleteBinaryTree(startValue:Int, depth:Int):BinaryTree={
+    depth match {
+      case dp if dp == 0 => Leaf
+      case dp => Node(createCompleteBinaryTree(startValue*2, dp-1),startValue, createCompleteBinaryTree(startValue*2 +1, dp-1))
     }
   }
-
-  type Dictionary = BinaryTree
-  def empty: Dictionary = Leaf
-  def insert(key: Int, dict: Dictionary): Dictionary = {
-    dict match {
-      case Leaf => BranchNode( Leaf,key, Leaf)
-      case BranchNode(lb,v, rb) if (key == v) =>
-        println(s"key ${key} already present")
-        dict
-      case BranchNode(lb,v, rb) if (key < v) =>
-        BranchNode( insert(key, lb),v, rb)
-      case BranchNode(lb,v, rb) if (key > v) =>
-        BranchNode(lb,v, insert(key, rb))
-    }
-  }
-
-
-  val arr = Array(1,2,3,4,5,6,7)
-  val bt=createBinaryTreeUsingArrayEments(arr)
-  println(bt)
-
-
-  //---------------------------------------------------------------
-  val myCompleteBinTree = createCompleteBinaryTree(1, 3);
-  println(myCompleteBinTree)
-  println(insert(10,myCompleteBinTree))
-
-  val myCompleteBinTree2 = createCompleteBinaryTree(2, 3);
-  println(myCompleteBinTree2)
-  println(insert(11,myCompleteBinTree2))
+  println("createCompleteBinaryTree :"+createCompleteBinaryTree(1,3))
 
 
 }
